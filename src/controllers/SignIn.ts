c5a9aca13c0a4bc5c8b054bfd5625312715678a1
murilo9 <murilohenriquematias@gmail.com>
2022-02-17 23:53:31 -0300

    sending userId on sign in response

--- src/controllers/SignIn.ts
+++ src/controllers/SignIn.ts
@@ -4,6 +4,7 @@ import IAssertiveController from "../types/IAssertiveController";
 import SignInRequest from "../types/SignInRequest";
 import jwt from 'jsonwebtoken';
 import { Request } from "express";
+import SignInResponse from "../types/SignInResponse";
 
 export default class SignInController extends Controller implements IAssertiveController {
   validator: (request: Request) => Promise<void>;
@@ -13,13 +14,17 @@ export default class SignInController extends Controller implements IAssertiveCo
     this.validator = validator;
   }
 
-  async handle(request: SignInRequest): Promise<string> {
+  async handle(request: SignInRequest): Promise<SignInResponse> {
     const { username } = request.body;
     const user = await getUserFromDb(username);
     const userId = user._id;
     const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
       expiresIn: 10800, // expires in 3 hours
     });
-    return token
+    return {
+      token,
+      userId: user._id,
+      username: user.username
+    }
   }
 }
